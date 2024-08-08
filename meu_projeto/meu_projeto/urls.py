be1836166d539e41bded7e612f3e8687f0b29e52
thicym <thiciany.custodio@escolar.ifrn.edu.br>
2024-08-08 07:52:45 -0300

    Alteração thicy

--- meu_projeto/meu_projeto/urls.py
+++ meu_projeto/meu_projeto/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('aurea/', views.aurea_view, name='aurea'),
+    path('thicy/', views.thicy_view, name='thicy'),
 ]                                                                   
